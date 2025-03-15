b2991a14820900145fe87663b71e321fac0d176d
cartagena <luisyugo1@gmail.com>
2025-03-14 21:43:51 -0600

    aaa

---
+++ js/loadevents.js
@@ -0,0 +1,19 @@
+function init(){
+    var divw1 = document.getElementById("titlew1");
+    if(divw1.addEventListener){
+    divw1.addEventListener("mousedown", function(event){
+    drag(this.parentNode, event);
+    }, false);
+    }
+    else if(divw1.attachEvent){
+    divw1.attachEvent("onmousedown", function(event){
+    drag(this.parentNode, event);
+    });
+    }
+    }
+    if(window.addEventListener){
+    window.addEventListener("load", init, false);
+    }
+    else if(windows.attachEvent){
+    window.attachEvent("onload", init);
+    }
